85328abda258582039c64cdc0d87f12ec972db2a
Frigg3r <nikita.ukhanoff@gmail.com>
2024-03-09 00:08:38 +0400

    message

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+      <div class="navbar">
+        <div class="navlist">
+          <a href="">О нас</a>
+          <a href="">Избранное</a>
+          <a href="">Корзина</a>
+          <a href="">Вход</a>
+        </div>
+
+      </div>
+    </body>
   );
 }
 
